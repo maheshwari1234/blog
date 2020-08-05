61b2768b3072afc031a23a6c3f1b2f094744c655
maheshwari1234 <68109585+maheshwari1234@users.noreply.github.com>
2020-08-05 17:53:28 +0530

    TextEditor component is added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -10,6 +10,7 @@ import Newpost from './components/newPost'
 import Search from './components/search'
 import SearchByWord from './components/serachByword'
 import IndpostUser from "./components/IndpostByUser"
+import TextEditor from "./components/RichTextEditor"
 
 const App = () => {
   return (
@@ -20,12 +21,12 @@ const App = () => {
           <Route exact path="/home" component={Home} />
           <Route exact path="/t/:tag" component={Search} />
           <Route exact path="/:name/:id/:Title" component={IndpostUser} />
-
           <Route exact path="/search/:word" component={SearchByWord} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/new" component={Newpost} />
           <Route exact path="/:id/:Title" component={Indpost} />
+          <Route exact path="/texteditor" component={TextEditor}/>
         </Switch>
       </BrowserRouter>
     </React.Fragment>
