3971fb72e621aa7497550c0742e53f64b009a74c
maheshwari1234 <68109585+maheshwari1234@users.noreply.github.com>
2020-08-12 17:21:18 +0530

    Other redo, undo etc.. functionalities implemented

--- Frontend/src/components/edit.js
+++ Frontend/src/components/edit.js
@@ -3,7 +3,7 @@ import React, { Component } from "react"
 import { Card, Button } from 'react-bootstrap'
 import {
     TypeBold, TypeItalic, TypeStrikethrough, Link, Fonts, ChatRightQuoteFill, Code,
-    ListUl, ListOl, Paperclip, ArrowClockwise, ArrowCounterclockwise
+    ListUl, ListOl, Paperclip, ArrowClockwise, ArrowCounterclockwise, TypeUnderline
 } from 'react-bootstrap-icons';
 
 
@@ -20,8 +20,39 @@ export default class YourComponent extends Component {
                 document.execCommand('italic', false, null)
                 break;
             case 'Strike':
-                document.execCommand('strikethrough', false, null)
+                document.execCommand('strikeThrough', false, null)
                 break;
+            case 'Link':
+                document.execCommand('createLink', false, "https://trix-editor.org/")
+                break;
+            case 'Underline':
+                document.execCommand('underline', false, null)
+                break;
+            case 'ListOl':
+                document.execCommand('insertOrderedList', false, null)
+                break;
+            case 'ListUl':
+                document.execCommand('insertUnorderedList', false, null)
+                break;
+            case 'Image':
+                // var container = document.getElementById("contain");
+                // var x = document.createElement("INPUT");
+                // x.setAttribute("type", "file");
+                // var pic=container.appendChild(x);
+                // console.log("image",x.value)
+                document.execCommand('insertImage', false, "simple.jfif")
+
+                break;
+            case 'Heading':
+                document.execCommand('fontSize', false, "5")
+                break;
+            case 'Undo':
+                document.execCommand('undo', false, null)
+                break;
+            case 'redo':
+                document.execCommand('redo', false, null)
+                break;
+
             default:
                 break;
         }
@@ -35,33 +66,33 @@ export default class YourComponent extends Component {
 
                         <div>
                             <fieldset>
-                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "5px", background: "lightgray" }} onClick={() => this.handleChange("Bold")}><TypeBold  style={{fontSize:"25px"}}/>
+                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "5px", background: "lightgray" }} onClick={() => this.handleChange("Bold")}><TypeBold style={{ fontSize: "25px" }} />
                                 </Button>
-                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("Italic")}><TypeItalic  style={{fontSize:"25px"}} />
+                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("Italic")}><TypeItalic style={{ fontSize: "25px" }} />
                                 </Button>
-                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("Strike")}><TypeStrikethrough  style={{fontSize:"25px"}}/>
+                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("Strike")}><TypeStrikethrough style={{ fontSize: "25px" }} />
                                 </Button>
-                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("Link")}><Link style={{fontSize:"25px"}}/>
+                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("Link")}><Link style={{ fontSize: "25px" }} />
                                 </Button>
-                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "7px", background: "lightgray" }} onClick={() => this.handleChange("Fonts")}><Fonts style={{fontSize:"25px"}}/>
+                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "7px", background: "lightgray" }} onClick={() => this.handleChange("Heading")}><Fonts style={{ fontSize: "25px" }} />
                                 </Button>
-                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("Quote")}><ChatRightQuoteFill style={{fontSize:"25px"}}/>
+                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("Underline")}><TypeUnderline style={{ fontSize: "25px" }} />
                                 </Button>
-                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("Code")}><Code style={{fontSize:"25px"}}/>
+                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("Code")}><Code style={{ fontSize: "25px" }} />
                                 </Button>
-                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("ListOl")}><ListOl style={{fontSize:"25px"}} />
+                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("ListOl")}><ListOl style={{ fontSize: "25px" }} />
                                 </Button>
-                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("ListUl")}><ListUl style={{fontSize:"25px"}}/>
+                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("ListUl")}><ListUl style={{ fontSize: "25px" }} />
                                 </Button>
-                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "10px", background: "lightgray" }} onClick={() => this.handleChange("PaperClip")}><Paperclip style={{fontSize:"25px"}} />
+                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "10px", background: "lightgray" }} onClick={() => this.handleChange("Image")}><Paperclip style={{ fontSize: "25px" }} />
                                 </Button>
-                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "10px", background: "lightgray" }} onClick={() => this.handleChange("Clock")}><ArrowClockwise style={{fontSize:"25px"}}/>
+                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "10px", background: "lightgray" }} onClick={() => this.handleChange("redo")}><ArrowClockwise style={{ fontSize: "25px" }} />
                                 </Button>
-                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("AntiClock")}><ArrowCounterclockwise style={{fontSize:"25px"}} />
+                                <Button variant="default" style={{ fontSize: "20px", marginLeft: "1px", background: "lightgray" }} onClick={() => this.handleChange("Undo")}><ArrowCounterclockwise style={{ fontSize: "25px" }} />
                                 </Button>
                             </fieldset>
                             <Card.Body>
-                                <div contenteditable="true" style={{ outline: "1px solid", height: "500px" }}>
+                                <div contenteditable="true" style={{ outline: "1px solid"}} id="contain">
                                 </div>
                             </Card.Body>
                         </div>
