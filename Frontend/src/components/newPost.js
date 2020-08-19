6b1391eb45b3b0e6b5e87b9397370db627ed1e35
maheshwari1234 <68109585+maheshwari1234@users.noreply.github.com>
2020-08-19 17:16:54 +0530

    Integrated with Richtexteditor component

--- Frontend/src/components/newPost.js
+++ Frontend/src/components/newPost.js
@@ -4,45 +4,62 @@ import '../App.css';
 import { NewpostAction } from '../Redux/action/newPostAction'
 import { useSelector, useDispatch } from 'react-redux';
 import { Redirect } from 'react-router-dom';
+import Rich from "./fornew"
+
 
 const NewPost = (props) => {
     const [inputs, setInputs] = useState({});
+    const [course, setCourse] = useState('')
+    const [title, setTitle] = useState('')
+
     const newDataSubmitted = useSelector((state) => state.newpost.data)
-  const username=useSelector((state)=>state.login.email)
-  const username1=useSelector((state)=>state.register.email)
+    const username = useSelector((state) => state.login.email)
+    const username1 = useSelector((state) => state.register.email)
 
     const dispatch = useDispatch();
 
     const handleChange = (event) => {
+        console.log("onchange", event.target.name,"value",event.target.value)
         event.persist();
         setInputs(inputs => ({ ...inputs, [event.target.name]: event.target.value }));
+
+    }
+    const selected=(e)=>{
+        console.log("value using blur",e.target.textContent)
+        setTitle(e.target.textContent)
+
+    }
+
+    const handleSelection = (e) => {
+console.log("select course",e.target.value)
+setCourse(e.target.value)
 
     }
 
     const submit = (e) => {
         e.preventDefault();
+        
         const newData = {
-
-            "Course": inputs.course,
-            "Title": inputs.title,
+            "Course":course,
+            "Title": title,
             "Brief": inputs.Brief,
             "Body": inputs.body,
             "Image": "comman.jpg"
 
         }
-        if(username!==undefined){
-        dispatch(NewpostAction(newData,username))
+        if (username !== undefined) {
+            dispatch(NewpostAction(newData, username))
 
 
         }
-        else{
-            dispatch(NewpostAction(newData,username1))
+        else {
+            dispatch(NewpostAction(newData, username1))
         }
 
 
     }
     if (newDataSubmitted) {
-    return <Redirect to="/?" />
+        return <Redirect to="/?" />
     }
 
     return (
@@ -60,22 +77,35 @@ const NewPost = (props) => {
                     </Nav>
                 </Navbar.Collapse>
             </Navbar><br />
-            <div className="row">
-                <div className="offset-2 col-sm-6">
+
+            <Rich /><br />
+            <div className="container">
+                <div className="offset-1 col-sm-9">
                     <Card>
                         <Card.Header>
                             <Form>
-                                <Form.Label style={{ fontWeight: "bold", fontSize: "20px" }}>Add Course Name here..</Form.Label>
+                                <Form.Label style={{ fontWeight: "bold", fontSize: "20px" }}>Select Course Name here..</Form.Label>
 
-                                <Form.Control as="textarea" type="text" rows="1" onChange={handleChange} name="course" />
+                                {/* <Form.Control as="textarea" type="text" rows="1" onChange={handleChange} name="course" /> */}
+                                {/* <p contentEditable="true" style={{ outline: "1px solid" }} onMouseUp={handleChange} name="course"></p> */}
+                                <Form.Control as="select" onChange={handleSelection}>
+                                    <option value=''>Select Course</option>
+                                    <option value="React">React</option>
+                                    <option value="Javascript">Javascript</option>
+                                    <option value="Bootstrap">Bootstrap</option>
 
+                                </Form.Control>
                             </Form>
                         </Card.Header>
                         <Card.Title>
                             <Form style={{ marginLeft: "20px" }}>
                                 <Form.Group>
-                                    <Form.Label style={{ fontWeight: "bold", fontSize: "40px", color: "gray" }}>New post title here...</Form.Label>
-                                    <Form.Control as="textarea" type="text" rows="2" onChange={handleChange} name="title" />
+                                    <Form.Label style={{ fontWeight: "bold", fontSize: "35px", color: "gray" }}>New post title here...</Form.Label>
+                                 {/* <Form.Control as="textarea" type="text" rows="2" onChange={handleChange} name="title" /> */}
+                                 <div contentEditable="true" style={{ outline: "1px solid" }}
+                                id="contain"  name="title" onKeyUp={selected} ></div>
+
+                            
                                 </Form.Group>
 
                                 <Form.Group>
@@ -93,7 +123,7 @@ const NewPost = (props) => {
                         <Card.Body>
                             <Form>
                                 <Form.Group>
-                                    <Form.Label style={{ fontWeight: "bold", fontSize: "20px" }} onChange={handleChange} value={inputs.body} name="body" >Write here Post content here..</Form.Label>
+                                    <Form.Label style={{ fontWeight: "bold", fontSize: "20px" }} onChange={handleChange} value={inputs.body} name="body" >Write your Post content here..</Form.Label>
                                     <Form.Control as="textarea" rows="6" type="text" onChange={handleChange} name="body" />
                                 </Form.Group>
                             </Form>
