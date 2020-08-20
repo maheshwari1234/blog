3e4696c263bb8f685f444b2695c23e0326bf2515
maheshwari1234 <68109585+maheshwari1234@users.noreply.github.com>
2020-08-20 18:01:10 +0530

    Profile component added

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -12,7 +12,9 @@ import SearchByWord from './components/serachByword'
 import IndpostUser from "./components/IndpostByUser"
 import TextEditor from "./components/RichTextEditor"
 import Edit from "./components/edit"
-
+import Mat from "./components/mat3"
+import Newmat from "./components/mat"
+import Profile from "./components/profile"
 
 const App = () => {
   return (
@@ -22,14 +24,19 @@ const App = () => {
           <Route exact path="/" component={Home} />
           <Route exact path="/home" component={Home} />
           <Route exact path="/t/:tag" component={Search} />
+          <Route exact path="/profile/:name" component={Profile}/>
+
           <Route exact path="/:name/:id/:Title" component={IndpostUser} />
           <Route exact path="/search/:word" component={SearchByWord} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <Route exact path="/new" component={Newpost} />
           <Route exact path="/:id/:Title" component={Indpost} />
-          <Route exact path="/texteditor" component={TextEditor}/>
+          <Route exact path="/texteditor" component={Mat}/>
           <Route exact path="/edit" component={Edit}/>
+          <Route exact path="/mat" component={Newmat}/>
+
+
 
         </Switch>
       </BrowserRouter>
